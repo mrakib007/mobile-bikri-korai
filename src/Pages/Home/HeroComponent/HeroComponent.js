1fa8657a3d74b258227c847e36b1cd4416a0f3da
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-12 18:46:27 +0600

    added theme color

--- src/Pages/Home/HeroComponent/HeroComponent.js
+++ src/Pages/Home/HeroComponent/HeroComponent.js
@@ -8,7 +8,8 @@ const HeroComponent = () => {
         <div className="hero height">
   <div className="hero-content flex-col lg:flex-row-reverse">
     {/* <img src={phoneHomepage} className="max-w-sm rounded-lg shadow-2xl" /> */}
-    <img src={phoneHomepage} className="max-w-xs rounded-lg shadow-2xl" />
+    {/* <img src={phoneHomepage} className="max-w-xs rounded-lg shadow-2xl" /> */}
+    <img src={phoneHomepage} className="lg:max-w-xs  w-96 rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
