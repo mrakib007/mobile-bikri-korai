4d9befea433ff0718dc54ce6e099dd66149931d5
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-16 15:45:17 +0600

    started firebase auth adding

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,55 @@
+// import React from 'react';
+
+// const Login = () => {
+//     return (
+//      <section className='lg:my-32 my-20'>
+//         <h1 className='text-5xl font-bold text-center lg:py-12 py-4'>Log In Now!</h1>
+//     <div className="flex justify-center ">
+        
+//     <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+//       <div className="card-body">
+//         <div className="form-control">
+//           <label className="label">
+//             <span className="label-text">Email</span>
+//           </label>
+//           <input type="text" placeholder="email" className="input input-bordered" />
+//         </div>
+//         <div className="form-control">
+//           <label className="label">
+//             <span className="label-text">Password</span>
+//           </label>
+//           <input type="text" placeholder="password" className="input input-bordered" />
+//           <label className="label">
+//             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+//           </label>
+//         </div>
+//         <div className="form-control mt-6">
+//           <button className="btn btn-primary">Login</button>
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//     </section>
+
+//     );
+// };
+
+// export default Login;
+
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
-    return (
-     <section className='lg:my-32 my-20'>
-        <h1 className='text-5xl font-bold text-center lg:py-12 py-4'>Log In Now!</h1>
-    <div className="flex justify-center ">
-        
-    <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+  return (
+    <div className="hero height bg-base-200  max-w-[1210px] mx-auto my-auto">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className="text-center">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      <p className="py-6 hidden lg:block">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p>Don't have an account? <Link to="/signup" className='text-primary'>Sign Up</Link> from here.</p>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
       <div className="card-body">
         <div className="form-control">
           <label className="label">
@@ -28,10 +71,9 @@ const Login = () => {
         </div>
       </div>
     </div>
-    </div>
-    </section>
-
-    );
+  </div>
+</div>
+  );
 };
 
 export default Login;
--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,37 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+      <div className="hero height bg-base-200  max-w-[1210px] mx-auto my-auto">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Sign Up From Here!</h1>
+          <p className="py-6 hidden lg:block">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p>Already have an account? <Link to="/login" className='text-primary'>Log In</Link> from here.</p>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+        <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="text" placeholder="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="text" placeholder="password" className="input input-bordered" />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
     );
 };
 
