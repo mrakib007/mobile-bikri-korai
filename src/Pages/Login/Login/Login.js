bcd54a5c1f9bc8b73fd84984cec7d15b23435838
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-17 00:01:58 +0600

    adding login and signup functionalities

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,46 +1,10 @@
-// import React from 'react';
-
-// const Login = () => {
-//     return (
-//      <section className='lg:my-32 my-20'>
-//         <h1 className='text-5xl font-bold text-center lg:py-12 py-4'>Log In Now!</h1>
-//     <div className="flex justify-center ">
-        
-//     <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-//       <div className="card-body">
-//         <div className="form-control">
-//           <label className="label">
-//             <span className="label-text">Email</span>
-//           </label>
-//           <input type="text" placeholder="email" className="input input-bordered" />
-//         </div>
-//         <div className="form-control">
-//           <label className="label">
-//             <span className="label-text">Password</span>
-//           </label>
-//           <input type="text" placeholder="password" className="input input-bordered" />
-//           <label className="label">
-//             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-//           </label>
-//         </div>
-//         <div className="form-control mt-6">
-//           <button className="btn btn-primary">Login</button>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-//     </section>
-
-//     );
-// };
-
-// export default Login;
-
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
+  const {register,handleSubmit,formState:{errors}} = useForm();
   return (
     <div className="hero height bg-base-200  max-w-[1210px] mx-auto my-auto">
   <div className="hero-content flex-col lg:flex-row">
@@ -49,13 +13,21 @@ const Login = () => {
       <p className="py-6 hidden lg:block">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <p>Don't have an account? <Link to="/signup" className='text-primary'>Sign Up</Link> from here.</p>
     </div>
+
+    
     <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+      <form onSubmit={handleSubmit()}>
       <div className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text"
+          {...register("email",{
+            required:"Email address is required"
+          })} 
+          placeholder="email" className="input input-bordered" />
+          {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
         </div>
         <div className="form-control">
           <label className="label">
@@ -69,8 +41,16 @@ const Login = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
+        <div class="divider">OR</div>
+        <div className="form-control mt-6">
+          <button className="btn btn-info">Continue with Google</button>
+        </div>
       </div>
+      </form>
     </div>
+
+
+
   </div>
 </div>
   );
--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -10,8 +10,17 @@ const SignUp = () => {
           <p className="py-6 hidden lg:block">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           <p>Already have an account? <Link to="/login" className='text-primary'>Log In</Link> from here.</p>
         </div>
+
+        
         <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+        <form action="">
           <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input type="text" placeholder="Name" className="input input-bordered" />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -27,11 +36,30 @@ const SignUp = () => {
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
             </div>
+            <div className="form-control w-full ">
+                            <label className="label"> <span className="label-text">Select Your Profile</span></label>
+                            <div className="input-group w-full">
+                                {/* <select {...register("role", {
+                                    required: true
+                                })}
+                                    className="select select-bordered">
+                                    <option>buyer</option>
+                                    <option>seller</option>
+                                </select> */}
+                            </div>
+                        </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Sign Up</button>
             </div>
-          </div>
+            <div class="divider">OR</div>
+        <div className="form-control mt-6">
+          <button className="btn btn-info">Continue with Google</button>
         </div>
+          </div>
+          </form>
+        </div>
+        
+
       </div>
     </div>
     );
