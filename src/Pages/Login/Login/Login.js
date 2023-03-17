245f2b0bf52d812b64d17b60fcf3b92a10133c04
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-17 09:27:58 +0600

    adding react hook form functionalities

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text"
+          <input type="email"
           {...register("email",{
             required:"Email address is required"
           })} 
@@ -33,13 +33,19 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="password"
+          {...register("password",{
+            required: "Password is required",
+            minLength: {value: 6, message: "Password must be 6 characters or longer"}
+          })} placeholder="password" className="input input-bordered" />
+         {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          {/* <button className="btn btn-primary">Login</button> */}
+          <input className='btn btn-primary 'value="Login" type="submit"></input>
         </div>
         <div class="divider">OR</div>
         <div className="form-control mt-6">
--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+  const {register,handleSubmit,formState:{errors}} = useForm();
     return (
       <div className="hero height bg-base-200  max-w-[1210px] mx-auto my-auto">
       <div className="hero-content flex-col lg:flex-row">
@@ -13,25 +15,40 @@ const SignUp = () => {
 
         
         <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
-        <form action="">
+        <form onSubmit={handleSubmit()}>
           <div className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
-              <input type="text" placeholder="Name" className="input input-bordered" />
+              <input type="text"
+              {...register("name",{
+                required: "Name is required"
+              })} placeholder="Name" className="input input-bordered" />
+              {errors.name && <p className='text-red-6000'>{errors.name.message}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="text" placeholder="email" className="input input-bordered" />
+              <input type="email"
+              {...register("email",{
+                required: "Email is required"
+              })} placeholder="email" className="input input-bordered" />
+              {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="text" placeholder="password" className="input input-bordered" />
+              <input type="password"
+              {...register("password",{
+                required: "Password is required",
+                minLength: {value:6, message: "Password must be 6 characters long"},
+                pattern: {value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message:"Password must contain an uppercase, a number and special character" }
+              })} placeholder="password" className="input input-bordered" />
+
+              {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
@@ -39,13 +56,13 @@ const SignUp = () => {
             <div className="form-control w-full ">
                             <label className="label"> <span className="label-text">Select Your Profile</span></label>
                             <div className="input-group w-full">
-                                {/* <select {...register("role", {
+                                <select {...register("role", {
                                     required: true
                                 })}
                                     className="select select-bordered">
                                     <option>buyer</option>
                                     <option>seller</option>
-                                </select> */}
+                                </select>
                             </div>
                         </div>
             <div className="form-control mt-6">
