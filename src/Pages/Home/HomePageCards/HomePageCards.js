eabc2281c02df93d90f4c7ef2609e4cd7d86c03a
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-21 11:00:10 +0600

    fixed homepage problems

--- src/Pages/Home/HomePageCards/HomePageCards.js
+++ src/Pages/Home/HomePageCards/HomePageCards.js
@@ -4,43 +4,43 @@ import phone1 from '../../../assets/images/phone1.jpg';
 import SingleCard from './SingleCard';
 
 const HomePageCards = () => {
-    // const {data: allCategories = []} = useQuery({
-    //     // queryKey: ['users'],
-    //     queryFn: async () =>{
-    //         const res = await fetch('http://localhost:5000/mobiles');
-    //         const data = await res.json();
-    //         return data;
-    //     }
-    // })
-    let cards = [
-        {
-            id: 1,
-            title: "New Phones",
-            img: phone1,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum temporibus nesciunt vero, sapiente id sequi tempore quidem maxime, eveniet aperiam iste excepturi, expedita eaque fuga ea illum deleniti repellendus!'
-        },
-        {
-            id: 2,
-            title: "New Phones",
-            img: phone1,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum temporibus nesciunt vero, sapiente id sequi tempore quidem maxime, eveniet aperiam iste excepturi, expedita eaque fuga ea illum deleniti repellendus!'
-        },
-        {
-            id: 3,
-            title: "New Phones",
-            img: phone1,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum temporibus nesciunt vero, sapiente id sequi tempore quidem maxime, eveniet aperiam iste excepturi, expedita eaque fuga ea illum deleniti repellendus!'
-        },
-    ]
+    const {data: allCategories = []} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () =>{
+            const res = await fetch('http://localhost:5000/mobiles');
+            const data = await res.json();
+            return data;
+        }
+    })
+    // let cards = [
+    //     {
+    //         id: 1,
+    //         title: "New Phones",
+    //         img: phone1,
+    //         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum temporibus nesciunt vero, sapiente id sequi tempore quidem maxime, eveniet aperiam iste excepturi, expedita eaque fuga ea illum deleniti repellendus!'
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "New Phones",
+    //         img: phone1,
+    //         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum temporibus nesciunt vero, sapiente id sequi tempore quidem maxime, eveniet aperiam iste excepturi, expedita eaque fuga ea illum deleniti repellendus!'
+    //     },
+    //     {
+    //         id: 3,
+    //         title: "New Phones",
+    //         img: phone1,
+    //         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum temporibus nesciunt vero, sapiente id sequi tempore quidem maxime, eveniet aperiam iste excepturi, expedita eaque fuga ea illum deleniti repellendus!'
+    //     },
+    // ]
     return (
         <div className='max-w-[1210px] mx-auto'>
             <h1 className='text-5xl font-bold'>Our Categories</h1>
         <div className='grid gap-4 mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {
-                cards.map(card=>
+                allCategories.map(category=>
                     <SingleCard
-                    key={card.id}
-                    card={card}>
+                    key={category.id}
+                    category={category}>
                     </SingleCard>
                 )
             }
--- src/Pages/Home/HomePageCards/SingleCard.js
+++ src/Pages/Home/HomePageCards/SingleCard.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const SingleCard = ({card}) => {
-    const {id,img,description,title} = card;
+const SingleCard = ({category}) => {
+    const {_id,category:brand,logo} = category;
   return (
     <div className="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
         <img
-          src={img}
+          src={logo}
           alt="Shoes"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
+        <h2 className="card-title">{brand}</h2>
+        {/* <p>{description}</p> */}
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">
+            <Link to={`/mobiles/${_id}`}>Buy Now </Link></button>
         </div>
       </div>
     </div>
