3e2259a92f35c18163f636799d99a85b0c01e1c4
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-26 12:20:49 +0600

    added table view

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -59,8 +59,50 @@ const MyProducts = () => {
         })
     }
     return (
-        <div>
-            
+        <div className='lg:mr-28 '>
+            <h1 className=' font-bold text-4xl mb-10'>My <span className='text-[#fb6230]'>Products</span></h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Sales Status</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products &&
+                            products?.map((product, i) => <tr key={product._id}>
+                                <th>{i + 1}</th>
+                                <th>
+                                    <button onClick={() => handleDelete(product._id)} className='btn btn-circle mt-1 '>X</button>
+                                </th>
+                                <td>{product.title}</td>
+                                <td>{product.resale_price}</td>
+                                {/* <td>
+                                {
+                                    booking.price && !booking.paid && <Link
+                                        to={`/dashboard/payment/${booking._id}`}
+                                    >
+                                        <button
+                                            className='btn btn-primary btn-sm'
+                                        >Pay</button>
+                                    </Link>
+                                }
+                                {
+                                    booking.price && booking.paid && <span className='text-green-500'>Paid</span>
+                                }
+                            </td> */}
+                                <td><button onClick={()=> handleStatusUpdate(product._id)} className='btn btn-sm'>{product.status ? product.status : 'Advertise'}</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
